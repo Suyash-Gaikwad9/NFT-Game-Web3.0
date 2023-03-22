977f92299f73df51720d643d50c6843126060486
suyash <suyash.gaikwad21@vit.edu>
2023-03-22 10:23:10 +0530

    Home Page of high order component is ready

--- client/src/components/pageHOC.jsx
+++ client/src/components/pageHOC.jsx
@@ -5,13 +5,26 @@ import styles from '../styles';
 
 const pageHOC = (Component, title, description) => () => {
     const navigate = useNavigate();
-  return (
-    <div className={styles.hocContainer}>
-        <div className={styles.hocContentBox}>
-            <img src={logo} alt="logo" className={styles.hocLogo} onClick={() => navigate('/')} />
+    return (
+        <div className={styles.hocContainer}>
+            <div className={styles.hocContentBox}>
+                <img src={logo} alt="logo" className={styles.hocLogo} onClick={() => navigate('/')} />
+                <div className={styles.hocBodyWrapper}>
+                    <div className="flex flex-row w-full">
+                        <h1 className={`flex ${styles.headText} head-text`}>{title}</h1>
+                    </div>
+
+                    <p className={`${styles.normalText} my-10`}>{description}</p>
+
+                    <Component />
+                </div>
+                <p className={styles.footerText}>Made By TY-C61</p>
+            </div>
+            <div className="flex flex-1">
+                <img src={heroImg} alt=" hero-img" className="w-full xl:h-full object-cover"/>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default pageHOC
--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -3,13 +3,13 @@ import { pageHOC } from '../components';
 const Home = () => {
   return (
     <div>
-      <h1 className="text-5xl p-3">Avax Gods</h1>
-      <h2 className="text-3xl p-3">Web3 NFT Battle-style Card Game</h2>
-      <p className="text-xl p-3">Made with 💜 by TY-C61</p>
+      
     </div>
   )
 };
 
 export default pageHOC(
   Home,
+  <>Welcome t Avax Gods <br /> a Web3 NFT Card Game</>,
+  <>Connect yout wallet to start paying <br /> the ultimate Web3 Battle Card Game</>
 );
